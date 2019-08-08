129134dc9d5c9748b814658548de855976e29c11
Adam Saladino <adam@codingsimply.com>
2019-08-08 14:49:35 -0400

    Pretty happy with this first sample version.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import SiteSearch from './SiteSearch';
 
 const siteSearchElement = document.getElementById('site-search');
 const baseUrl = siteSearchElement.getAttribute('data-base-url');
+const appUrl = siteSearchElement.getAttribute('data-app-url');
 const domain = siteSearchElement.getAttribute('data-domain');
+const siteName = siteSearchElement.getAttribute('data-site-name');
 const limit = siteSearchElement.getAttribute('data-limit');
 const openInNewWindow = siteSearchElement.getAttribute('data-open-in-new-window');
 
 ReactDOM.render(<SiteSearch baseUrl={baseUrl}
                             domain={domain}
+                            siteName={siteName}
+                            appUrl={appUrl}
                             openInNewWindow={openInNewWindow === 'true'}
                             limit={limit}/>, siteSearchElement);
